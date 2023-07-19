a9868c35dcfd9f072fd94c99cacf88933fe5ba32
krisna31 <krisnaaaputraaa@gmail.com>
2023-07-19 08:49:39 +0000

    public app profile

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict'
 const express = require('express')
+const path = require('path');
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello, world! Welcome to App Engine!')
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'public/index.html');
+    res.sendFile(filePath);
+  });
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server is up and listening at port: ${PORT}`)
